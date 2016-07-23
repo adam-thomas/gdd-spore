1efc8be4d3a8ef51a24bfebb30a84a118d9933df
Tom Firth <tom@stockopedia.com>
2016-07-23 14:07:33 +0100

    adding in camera tracking

--- main.js
+++ main.js
@@ -6,17 +6,22 @@
 var scene = document.querySelector('#main-scene');
 var sphere = document.querySelector('#sphere');
 
+
 for (var i = 0; i <= 100; i++) {
 	var sphereClone = sphere.cloneNode(true);
-
-	var x = Math.random()*50;
+	var x = Math.random()*100;
 	var y = Math.random()*5;
-	var z = Math.random()*50;
+	var z = Math.random()*100;
+
 	var position = x + " " + y + " " + z;
 	var radius = Math.random()*2;
 	sphereClone.setAttribute("position", position);
 	sphereClone.setAttribute("radius", radius);
-
 	scene.appendChild(sphereClone);
-	console.log(position);
 }
+
+document.querySelector('[camera]').addEventListener('componentchanged', function(evt) {
+	if (evt.detail.name === 'position') {
+		console.log(evt.detail.newData);
+	}
+});
